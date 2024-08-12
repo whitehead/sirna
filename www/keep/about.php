758d9fbe108ba81a6b6170437786cd57f5664c27
BingbingYuan <BingbingYuan@users.noreply.github.com>
2024-08-12 13:14:46 -0400

    Add files via upload

---
+++ www/keep/about.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>
+help
+</title>
+</head>
+<body>
+<h3 align="center">siRNA Selection Program</h3>
+This site helps you select siRNAs to knock down your gene of interest. After entering your sequence or accession/gi number, choosing pattern for your oligo nucleotides and various filter methods, you will be presented with a list of oligos matching your criteria. From this list, select those oligos you would like to consider further. All the selected oligos will be BLASTed against RefSeq, UniGene, or Ensembl database.  In the BLAST result page, siRNAs can be further filtered to reduce the non-specific target effects.<p />
+Please click <a href="FAQ.html#design">here</a> for rules on designing good siRNAs.
+</body>
+</html>
---
+++ www/keep/example.html
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>
+example
+</title>
+</head>
+<body>
+<h3 align="center">EXAMPLE</h3>
+<p />
+
+<ul>
+<li>Step one:<p />
+<CENTER><IMG alt='step 1'  SRC='step1.jpg'></CENTER></li>
+<hr />
+
+<li>Step two:<p />
+<CENTER><IMG alt='step 2' SRC='step2.jpg'></CENTER></li>
+<hr />
+
+<li>Step three:<p />
+<CENTER><IMG  alt='step 3' SRC='step3.jpg'></CENTER></li>
+<hr />
+
+<p>The top part of an siRNA's BLAST result:</p>
+<p><CENTER><IMG  alt='blast' SRC='step4.jpg'></CENTER></p>
+<hr />
+
+<li>After filtering:<p />
+<CENTER><IMG  alt='sirna' SRC='step5.jpg'></CENTER></li>
+<hr />
+
+<li>Selected siRNAs:<p />
+<CENTER><IMG  alt='sirna' SRC='step6.jpg'></CENTER></li>
+<hr />
+
+</ul>
+
+</body>
+</html>
